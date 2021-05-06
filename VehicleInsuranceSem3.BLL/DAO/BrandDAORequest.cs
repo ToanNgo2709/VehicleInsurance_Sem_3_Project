6e9d83bc40815bcff0bfce91a8590199d0d0cc34
ToanNgo2709 <ngotoanlibra@gmail.com>
2021-05-06 09:20:52 +0700

    edit dao request

--- VehicleInsuranceSem3.BLL/DAO/BrandDAORequest.cs
+++ VehicleInsuranceSem3.BLL/DAO/BrandDAORequest.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VehicleInsuranceSem3.BLL.Repository;
+using VehicleInsuranceSem3.BLL.ViewModel;
 
 namespace VehicleInsuranceSem3.BLL.DAO
 {
-    class BrandDAORequest
+    public class BrandDAORequest : ICrudFeature<BrandViewModel>
     {
+        public int Add(BrandViewModel newItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<BrandViewModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<BrandViewModel> GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(int id, BrandViewModel updateItems)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
