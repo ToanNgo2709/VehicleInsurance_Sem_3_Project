f1e38d02728c52b8b04d883de1e6a4024cdbf827
ToanNgo2709 <ngotoanlibra@gmail.com>
2021-05-06 08:56:43 +0700

    add password security for Utilities Layer

---
+++ VehicleInsuranceSem3.BLL/Repository/ICrudFeature.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleInsuranceSem3.BLL.Repository
+{
+    public interface ICrudFeature<Type>
+    {
+        int Add(Type newItem);
+        int Update(int id, Type updateItems);
+        int Delete(int id);
+
+        List<Type> GetAll();
+        List<Type> GetById(int Id);
+
+
+    }
+}
