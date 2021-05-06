4cd320fe139e492ca698360dd9778c84fcb3580f
ToanNgo2709 <ngotoanlibra@gmail.com>
2021-05-06 09:19:30 +0700

    add viewmodel and dao

---
+++ VehicleInsuranceSem3.BLL/DAO/BrandDAORequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleInsuranceSem3.BLL.DAO
+{
+    class BrandDAORequest
+    {
+    }
+}
