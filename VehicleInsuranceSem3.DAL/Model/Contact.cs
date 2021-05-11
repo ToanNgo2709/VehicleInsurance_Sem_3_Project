281d45319de081fcf8827104952f1f5c2c1775bd
ToanNgo2709 <ngotoanlibra@gmail.com>
2021-05-11 08:16:05 +0700

    add contact feature

--- VehicleInsuranceSem3.DAL/Model/Contact.cs
+++ VehicleInsuranceSem3.DAL/Model/Contact.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace VehicleInsuranceSem3.DAL.Model
 {
+    [Table("Contact")]
     public partial class Contact
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
--- VehicleInsuranceSem3/Areas/ClientArea/Controllers/HomePageController.cs
+++ VehicleInsuranceSem3/Areas/ClientArea/Controllers/HomePageController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VehicleInsuranceSem3.BLL.DAO;
+using VehicleInsuranceSem3.BLL.ViewModel;
+using VehicleInsuranceSem3.DAL.Model;
 
 namespace VehicleInsuranceSem3.Areas.ClientArea.Controllers
 {
@@ -22,6 +25,31 @@ namespace VehicleInsuranceSem3.Areas.ClientArea.Controllers
         public ActionResult Contact()
         {
             return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult AddContact()
+        {
+            string name = Request.Params["Name"];
+            string email = Request.Params["Email"];
+            string website = Request.Params["Website"];
+            string message = Request.Params["Message"];
+
+            if(name != null && email != null && message != null)
+            {
+                ContactViewModel newContact = new ContactViewModel()
+                {
+                    Email = email,
+                    Message = message,
+                    Name = name,
+                    Website = website
+                };
+
+                ContactDAORequest dao = new ContactDAORequest();
+                dao.Add(newContact);
+            }
+            return RedirectToAction("Contact");
         }
     }
 }
