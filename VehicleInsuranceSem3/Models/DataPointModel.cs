6db1a32501a0aee7bb802771680fd1775746791d
ToanNgo2709 <ngotoanlibra@gmail.com>
2021-05-12 20:09:52 +0700

    add model for datapoint chart

---
+++ VehicleInsuranceSem3/Models/DataPointModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace VehicleInsuranceSem3.Models
+{
+    [DataContract]
+    public class DataPointModel
+    {
+        public DataPointModel(string x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+               
+        }
+
+        [DataMember(Name = "X")]
+        public string X = "";
+
+        [DataMember(Name = "Y")]
+        public Nullable<double> Y = null;
+
+    }
+}
